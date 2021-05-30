daa3525220fbcb6e39d71e351f6cd3af3e61eeb9
dab246 <tdvu@linagora.com>
2021-05-30 15:43:02 +0700

    Demo app chat use Adhara Socket IO

---
+++ example/lib/routes/Routes.dart
@@ -0,0 +1,17 @@
+import 'package:example/pages/ChatScreen.dart';
+import 'package:example/pages/ChatUsersScreen.dart';
+import 'package:example/pages/LoginScreen.dart';
+
+class Routes {
+  static routes() {
+    return {
+      LoginScreen.ROUTE_ID: (context) => LoginScreen(),
+      ChatUsersScreen.ROUTE_ID: (context) => ChatUsersScreen(),
+      ChatScreen.ROUTE_ID: (context) => ChatScreen(),
+    };
+  }
+
+  static initScreen() {
+    return LoginScreen.ROUTE_ID;
+  }
+}
